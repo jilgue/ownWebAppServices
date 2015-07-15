1445f758ec03aabdf85837b4d2a7da24fb29b74a
César M. Cristóbal <jilgue@gmail.com>
2015-07-15 10:12:46 +0200

    Valores por defecto de uploaded

--- Images/classes/Image.php
+++ Images/classes/Image.php
@@ -8,14 +8,8 @@ class Image extends DBObject {
 	// ODMPPHE
 	static $table = "images";
 
-	function getCreateDateDefaultValue($params) {
-
-		return date("Y-m-d", time());
-	}
-
 	function getUploadedDefaultValue($params) {
 
 		return "0";
 	}
-
 }
--- Videos/classes/Videos.php
+++ Videos/classes/Videos.php
@@ -7,4 +7,9 @@ class Videos extends DBObject {
 
 	// ODMPPHE
 	static $table = "videos";
+
+	function getUploadedDefaultValue($params) {
+
+		return "0";
+	}
 }
