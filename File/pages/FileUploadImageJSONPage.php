0b757488abd643284aadacfe6eb6e7c606af15ea
César M. Cristóbal <jilgue@gmail.com>
2015-08-01 12:15:07 +0200

    Subida de fotos desde webapp

---
+++ File/pages/FileUploadImageJSONPage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Dispatching de URLs
+ */
+class FileUploadImageJSONPage extends APIJSONPage {
+
+	static $objField = array("file" => ".*",
+				 "idUser" => ".*",
+				 );
+
+	protected function __construct($params = array()) {
+
+		// MEGA TRAPI
+		$params["token"] = "ce6cf1c81095f72fa961ece85db5f5eb0a1509fa";
+		parent::__construct($params);
+	}
+
+	protected function _getResponse() {
+
+		preg_match("/data:image\/([a-z]{1,});base64,/", $this->file->dataURL, $type);
+		$type = $type[1];
+		$data = base64_decode(str_replace('data:image/jpeg;base64,', '', $this->file->dataURL));
+		$file = "/tmp/" . time() . "-" . $this->idUser . "-webapp." . $type;
+		return file_put_contents($file, $data);
+	}
+}
