3660e591a3a697c725a563324399ada3faa050b0
César M. Cristóbal <jilgue@gmail.com>
2016-01-08 20:32:05 +0100

    User class, _stCreate and stLogin

--- User/classes/User.php
+++ User/classes/User.php
@@ -5,6 +5,20 @@
  */
 class User extends DBObject {
 
-	// ODMPPHE
-	static $table = "users";
+	protected static function _stCreate($params) {
+
+		// Ya han sido validados asi que debería de existir
+		$params["password"] = sha1($params["password"]);
+
+		return parent::_stCreate($params);
+	}
+
+	static function stLogin($nick, $password) {
+
+		$password = sha1($password);
+
+		$query = "select * from user where nick = '$nick' AND password = '$password'";
+
+		return (bool) DBMySQLConnection::stVirtualConstructor()->query($query)->num_rows;
+	}
 }
