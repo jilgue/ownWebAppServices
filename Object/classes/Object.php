70034e76e4d9927bc7e892b33a1393f0909bfcc6
César M. Cristóbal <jilgue@gmail.com>
2016-07-17 21:35:15 +0200

    Tab and indexed

--- Object/classes/Object.php
+++ Object/classes/Object.php
@@ -5,215 +5,215 @@
  */
 abstract class Object {
 
-	// Se guarda toda la jerarquia de la clase
-	var $hierarchy = array();
+    // Se guarda toda la jerarquia de la clase
+    var $hierarchy = array();
 
-	public $objFields = array();
+    public $objFields = array();
 
-	var $params = array();
+    var $params = array();
 
-	// Sirve para saber si el objecto esta correcto
-	var $ok = true;
+    // Sirve para saber si el objecto esta correcto
+    var $ok = true;
 
-	protected function __construct($params = array()) {
+    protected function __construct($params = array()) {
 
-		$this->_loadHierarchy();
+        $this->_loadHierarchy();
 
-		$this->_autoloadParams($params);
-	}
+        $this->_autoloadParams($params);
+    }
 
-	private function _loadHierarchy() {
+    private function _loadHierarchy() {
 
-		$class = get_class($this);
-		$this->hierarchy = Object::stGetHierarchy($class);
-	}
+        $class = get_class($this);
+        $this->hierarchy = Object::stGetHierarchy($class);
+    }
 
-	private function _autoloadParams($params) {
+    private function _autoloadParams($params) {
 
-		if (is_array($params)) {
-			foreach ($params as $param => $value) {
+        if (is_array($params)) {
+            foreach ($params as $param => $value) {
 
-				$this->$param = $value;
-			}
-		}
+                $this->$param = $value;
+            }
+        }
 
-		$this->params = $params;
+        $this->params = $params;
 
-		foreach ($this->objFields as $objField => $value) {
-			if (!isset($this->$objField)
-			    // Si es un DT no se autocarga
-			    && !isset($value["DT"])) {
-				$this->$objField = $value;
-			}
-		}
-	}
+        foreach ($this->objFields as $objField => $value) {
+            if (!isset($this->$objField)
+                // Si es un DT no se autocarga
+                && !isset($value["DT"])) {
+                $this->$objField = $value;
+            }
+        }
+    }
 
-	final static private function _stFinalConstruct($object, $params = array()) {
+    final static private function _stFinalConstruct($object, $params = array()) {
 
-		// El objecto esta mal y no viene ya sin parámetros, lo devolvemos vacio
-		if ($object->ok === false
-		    && count($params) != 0) {
-			$class = get_class($object);
-			return $class::stVirtualConstructor();
-		}
+        // El objecto esta mal y no viene ya sin parámetros, lo devolvemos vacio
+        if ($object->ok === false
+            && count($params) != 0) {
+            $class = get_class($object);
+            return $class::stVirtualConstructor();
+        }
 
-		return $object;
-	}
+        return $object;
+    }
 
-	static function stVirtualConstructor($params = array()) {
+    static function stVirtualConstructor($params = array()) {
 
-		$class = get_called_class();
-		$args = func_get_args();
-		$numArgs = count($args);
+        $class = get_called_class();
+        $args = func_get_args();
+        $numArgs = count($args);
 
-		if ($numArgs == 1) {
-			if (is_array($args[0])) {
+        if ($numArgs == 1) {
+            if (is_array($args[0])) {
 
-				if ($args[0] == $params) {
-					return Object::_stFinalConstruct(new $class($params), $params);
-				}
+                if ($args[0] == $params) {
+                    return Object::_stFinalConstruct(new $class($params), $params);
+                }
 
-				// Ok, nos han llamado de la forma "normal": un único parámetro de tipo array
-				return Object::_stFinalConstruct(new $class(array_merge($args[0], $params)), $params);
-			}
-		}
+                // Ok, nos han llamado de la forma "normal": un único parámetro de tipo array
+                return Object::_stFinalConstruct(new $class(array_merge($args[0], $params)), $params);
+            }
+        }
 
-		if ($numArgs == 0) {
-			return Object::_stFinalConstruct(new $class($params), $params);
-		}
+        if ($numArgs == 0) {
+            return Object::_stFinalConstruct(new $class($params), $params);
+        }
 
-		// Si no es así pasamos todos los parametros y cada clase sabra que hacer con ellos, por el bien que le trae xD
-		if (is_array($params)) {
-			return Object::_stFinalConstruct(new $class(array_merge($args, $params)), $params);
-		}
+        // Si no es así pasamos todos los parametros y cada clase sabra que hacer con ellos, por el bien que le trae xD
+        if (is_array($params)) {
+            return Object::_stFinalConstruct(new $class(array_merge($args, $params)), $params);
+        }
 
-		return Object::_stFinalConstruct(new $class($args), $params);
-	}
+        return Object::_stFinalConstruct(new $class($args), $params);
+    }
 
-	static function stGetObjFields() {
+    static function stGetObjFields() {
 
-		$class = get_called_class();
+        $class = get_called_class();
 
-		$objFields = array();
-		foreach (Object::stGetHierarchy($class) as $class) {
+        $objFields = array();
+        foreach (Object::stGetHierarchy($class) as $class) {
 
-			$reflex = new ReflectionClass($class);
+            $reflex = new ReflectionClass($class);
 
-			$properties = $reflex->getDefaultProperties();
+            $properties = $reflex->getDefaultProperties();
 
-			$objFields = array_merge($objFields, $properties["objFields"]);
-		}
-		return $objFields;
-	}
+            $objFields = array_merge($objFields, $properties["objFields"]);
+        }
+        return $objFields;
+    }
 
-	static function stGetHierarchy($class) {
+    static function stGetHierarchy($class) {
 
-		$ret = array($class);
+        $ret = array($class);
 
-		while (($class = get_parent_class($class)) !== false) {
-			$ret[] = $class;
-		}
+        while (($class = get_parent_class($class)) !== false) {
+            $ret[] = $class;
+        }
 
-		return $ret;
-	}
+        return $ret;
+    }
 
-	/**
-	 * Lo usamos para definir getter y setters automáticamente
-	 */
-	public function __call($method, $arguments) {
+    /**
+     * Lo usamos para definir getter y setters automáticamente
+     */
+    public function __call($method, $arguments) {
 
-		if (preg_match("#^(.+)_cached$#", $method, $matches)) {
-			// Funcion que va ir cacheada
-			return $this->_getCachedFuncion($matches[1], $arguments);
-		}
+        if (preg_match("#^(.+)_cached$#", $method, $matches)) {
+            // Funcion que va ir cacheada
+            return $this->_getCachedFuncion($matches[1], $arguments);
+        }
 
-		return $this->_callSetGet($method, $arguments);
-	}
+        return $this->_callSetGet($method, $arguments);
+    }
 
-	private function _callSetGet($method, $arguments) {
+    private function _callSetGet($method, $arguments) {
 
-		if (!preg_match("#^(get|set|_get|_set)(.+)$#", $method, $matches)) {
-			// Método desconocido
-			return null;
-		}
+        if (!preg_match("#^(get|set|_get|_set)(.+)$#", $method, $matches)) {
+            // Método desconocido
+            return null;
+        }
 
-		$op = $matches[1];
-		$capturedField = lcfirst($matches[2]);
+        $op = $matches[1];
+        $capturedField = lcfirst($matches[2]);
 
-		if (!isset($this->$capturedField)) {
-			return null;
-		}
+        if (!isset($this->$capturedField)) {
+            return null;
+        }
 
-		switch ($op) {
-		case "get":
-		case "_get":
-			if (count($arguments) != 0) {
-				return null;
-			}
+        switch ($op) {
+            case "get":
+            case "_get":
+                if (count($arguments) != 0) {
+                    return null;
+                }
 
-			return $this->_getter($capturedField);
+                return $this->_getter($capturedField);
 		break;
 
-		case "set":
-		case "_set":
-			if (count($arguments) != 1) {
-				return null;
-			}
+            case "set":
+            case "_set":
+                if (count($arguments) != 1) {
+                    return null;
+                }
 
-			return $this->_setter($capturedField, $arguments[0]);
+                return $this->_setter($capturedField, $arguments[0]);
 		break;
 
-		default:
-			return null;
-			break;
-		}
-	}
+            default:
+                return null;
+                break;
+        }
+    }
 
-	private function _getter($field) {
+    private function _getter($field) {
 
-		if (isset($this->$field)) {
-			return $this->$field;
-		}
+        if (isset($this->$field)) {
+            return $this->$field;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private function _setter($field, $value) {
+    private function _setter($field, $value) {
 
-		if (isset($this->$field)) {
-			$this->$field = $value;
-			return (bool) $this->$field === $value;
-		}
-		return false;
-	}
+        if (isset($this->$field)) {
+            $this->$field = $value;
+            return (bool) $this->$field === $value;
+        }
+        return false;
+    }
 
-	function multiSetter($params) {
+    function multiSetter($params) {
 
-		foreach ($params as $field => $value) {
-			if (!$this->_setter($field, $value)) {
-				return false;
-			}
-		}
+        foreach ($params as $field => $value) {
+            if (!$this->_setter($field, $value)) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private function _getCachedFuncion($function, $args) {
+    private function _getCachedFuncion($function, $args) {
 
-		static $stCache = array();
+        static $stCache = array();
 
-		// TODO pasar en args el tiempo de cache y quitar de los args de la funcion
-		$cacheId = md5(serialize($function) . serialize($args));
+        // TODO pasar en args el tiempo de cache y quitar de los args de la funcion
+        $cacheId = md5(serialize($function) . serialize($args));
 
-		// TODO cachear de verdad
-		if (isset($stCache[$cacheId])) {
-			return $stCache[$cacheId];
-		}
+        // TODO cachear de verdad
+        if (isset($stCache[$cacheId])) {
+            return $stCache[$cacheId];
+        }
 
-		$ret = call_user_func_array(array($this, $function), $args);
+        $ret = call_user_func_array(array($this, $function), $args);
 
-		$stCache[$cacheId] = $ret;
+        $stCache[$cacheId] = $ret;
 
-		return $ret;
-	}
+        return $ret;
+    }
 }
